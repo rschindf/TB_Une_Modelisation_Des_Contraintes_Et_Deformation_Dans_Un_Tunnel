f70d08e92fb17fdddc80e234fdf7f9f37f870324
rschindf <schindfessel.robin@gmail.com>
2024-06-16 08:47:23 +0200

    Add files via upload

---
+++ Figures_Matlab/geol.m
@@ -0,0 +1,30 @@
+function e = geol(roche)
+% fonction pour assigner des valeur géologique
+% remodifier e -> eta_geol
+if roche == "calcaire"
+	e = 1e16;
+end
+if roche == "air"
+	e = 1.75e-5;
+end
+if roche == "béton"
+	e = 2e15;
+end
+if roche == "granite"
+	e = 1e20;
+end
+if roche == "schiste"
+	e = 5e11;
+end
+if roche == "évaporites"
+	e = 1e18;
+end 
+if roche == "quartzite"
+	e = 1e19;
+end
+if roche == "gneiss"
+	e = 7.5e20;
+end
+if roche == "bois"
+	e = 1e7;
+end
